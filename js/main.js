daa082cd159b864cc804fa4c9b953512a5655327
RoninsWork <yurkaronin@gmail.com>
2022-06-28 13:08:02 +0300

    Разработал первое приложение для заметок в упрощённом варианте

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
 var app = new Vue({
-  // передаём идентификатор блока выделенного под приложение
   el: '#app',
   data: {
-    title: 'v-if',
-    someText: false,
-    text: '',
-    lowerText: ''
+    title: 'Мои заметки',
+    message: null,
+    note: {
+      title: '',
+      description: '',
+      date: ''
+    },
+    notes: [
+      {
+        title: 'SvelteJS - набирает обороты',
+        description: 'Сегодня он проще и быстрее чем любой другой JS фреймворк для веба.И на нем можно делать все - сайты, виджеты, мобильные приложения или десктоп приложения',
+        date: new Date(Date.now()).toLocaleString()
+      },
+      {
+        title: 'Пробую реанимировать канал',
+        description: 'переосмыслить? пока планирую писать заметки, больше для себя, чтобы структурировать мысли, если кому то понравится - хорошо, если что то получится - еще лучше...',
+        date: new Date(Date.now()).toLocaleString()
+      },
+      {
+        title: 'Создание favicon для сайта',
+        description: 'Favicon играет важную роль в веб-приложениях. Он может улучшить внешний вид сайта в выдаче, повышает узнаваемость вашего сайта, помогает пользователю быстро найти...',
+        date: new Date(Date.now()).toLocaleString()
+      },
+
+    ]
   },
   methods: {
-    getText () {
-      console.log(this.text);
+    addNote () {
+      let {title, description} = this.note;
 
+      if (title === '') {
+        this.message = 'Заголовок заметки не может быть пустым!'
+        return false;
+      }
+
+      this.notes.push({
+        title,
+        description,
+        date: new Date(Date.now()).toLocaleString()
+      })
+
+      this.message = null
+      this.note.title = ''
+      this.note.description = ''
     }
   },
   computed: {
